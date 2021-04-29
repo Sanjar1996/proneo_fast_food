a433816e0b646f5366c0bbe4c9209094e8d7b6e5
Sanjar1996 <xabibovsanjarbek@gmail.com>
2021-04-29 15:29:58 +0500

    news

--- fast_food/catalog/telegram_bot.py
+++ fast_food/catalog/telegram_bot.py
@@ -1,17 +1,18 @@
 import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext,MessageHandler,Filters
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 
 def hello(update, context):
     print(update.message.user)
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-def main():
-    updater=Updater("1769249243:AAHDyKX-QmuITwSxKqtbqpNtB0sVSu_dzaY")
-    dispatcher=updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('start',hello))
+def main():
+    updater = Updater("1769249243:AAHDyKX-QmuITwSxKqtbqpNtB0sVSu_dzaY")
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', hello))
 
     updater.start_polling()
     updater.idle()
